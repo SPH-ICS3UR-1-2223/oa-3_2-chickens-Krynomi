89b3eb81caa5da54de9a1eb3acf9bd5184c33592
Krynomi <99208590+Krynomi@users.noreply.github.com>
2022-09-21 14:13:53 -0400

    Finished

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,20 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
-        
+    	
+    	//Monday
+    	int chickenCount = 8;
+    	int eggsPerChicken = 4;
+    	int totalEggs = (chickenCount * eggsPerChicken);
+    	
+    	//Tuesday
+    	chickenCount++;
+    	totalEggs+=chickenCount * eggsPerChicken;
+    	
+    	//Wednesday
+    	chickenCount = chickenCount/2;
+    	totalEggs+=chickenCount * eggsPerChicken;
+    
         System.out.println(totalEggs);
     }   
 }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,7 +1,25 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
+    	double eggcount = 0;
+    	double dailyAverage = 0;
+    	double monthlyAverage = 0;
+    	double monthlyProfit = 0;
+    	
+    	//Monday
+    	eggcount+= 100;
+    	
+    	//Tuesday
+    	eggcount+= 121;
+    	
+    	//Wednesday
+    	eggcount+= 117;
+    	
+    	dailyAverage= eggcount/3;
+    	monthlyAverage = dailyAverage * 30;
+    	monthlyProfit = monthlyAverage * 0.18;
+    	
+    	
         System.out.println("Daily Average:   " +dailyAverage);
         System.out.println("Monthly Average: " +monthlyAverage);
         System.out.println("Monthly Profit:  $" +monthlyProfit);
